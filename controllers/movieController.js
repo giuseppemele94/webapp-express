1373eefa718f3a4cf979841dd9a9cb58fa9f334e
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-02-20 16:07:22 +0100

    query in function index per mostrare la lista dei film

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,9 +3,16 @@ const connection = require('../data/db');
 
 // funzione di index
 function index(req, res) {
-    console.log("rotta index");
-    res.send("<h1>Rotta movies index </h1>")
-    
+
+    //query SQL 
+    const sql = 'SELECT * FROM movies';
+
+    //eseguo la query 
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        res.json(results);
+
+    });
 }
 // funzione di show
 function show(req, res) {
