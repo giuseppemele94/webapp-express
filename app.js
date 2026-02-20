72cbb626c05c7e0e48f15067d2d3543a72bcf1c4
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-02-20 15:36:06 +0100

    Inseriti i dati di connessione al database come variabili d’ambiente

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+const connection = require('./data/db');
+
 
 
 // attivazione della cartella public per uso file statici
